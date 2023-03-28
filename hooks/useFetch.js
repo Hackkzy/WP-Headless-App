b473fd651a71e6cf15e420968350fb5dda6a3054
Jay Patel <pjay9981@gmail.com>
2023-03-29 01:16:59 +0530

    Env file setup

--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -1,41 +1,42 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { WP_SITE_URL } from "@env";
 
 const useFetch = (endpoint, query) => {
-	const [data, setData] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-	const options = {
-		method: "GET",
-		url: `https://bhargavb.com/wp-json/wp/v2/${endpoint}`,
-		params: { ...query },
-	};
+  const options = {
+    method: "GET",
+    url: `${WP_SITE_URL}/wp-json/wp/v2/${endpoint}`,
+    params: { ...query },
+  };
 
-	const fetchData = async () => {
-		setIsLoading(true);
-		try {
-			const response = await axios.request(options);
-			setData(response.data);
-			setIsLoading(false);
-		} catch (error) {
-			setError(error);
-			console.log(error);
-		} finally {
-			setIsLoading(false);
-		}
-	};
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.request(options);
+      setData(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-	useEffect(() => {
-		fetchData();
-	}, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-	const refetch = () => {
-		setIsLoading(true);
-		fetchData();
-	};
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
 
-	return { data, isLoading, error, refetch };
+  return { data, isLoading, error, refetch };
 };
 
 export default useFetch;
