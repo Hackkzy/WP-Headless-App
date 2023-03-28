61023993a9e142dd422c22f8995338053189a3c3
Jay Patel <pjay9981@gmail.com>
2023-03-29 01:09:18 +0530

    Added Utility functions

--- utils/index.js
+++ utils/index.js
@@ -1,10 +1,21 @@
 export const checkImageURL = (url) => {
-	if (!url) return false;
-	else {
-		const pattern = new RegExp(
-			"^https?:\\/\\/.+\\.(png|jpg|jpeg|bmp|gif|webp)$",
-			"i"
-		);
-		return pattern.test(url);
-	}
+  if (!url) return false;
+  else {
+    const pattern = new RegExp(
+      "^https?:\\/\\/.+\\.(png|jpg|jpeg|bmp|gif|webp)$",
+      "i"
+    );
+    return pattern.test(url);
+  }
+};
+
+export const encodeHTMLEntities = (rawStr) => {
+  return rawStr.replace(/[\u00A0-\u9999<>\&]/g, (i) => `&#${i.charCodeAt(0)};`);
+};
+
+export const decodeHTMLEntities = (rawStr) => {
+  return rawStr.replace(
+    /&#(\d+);/g,
+    (match, dec) => `${String.fromCharCode(dec)}`
+  );
 };
