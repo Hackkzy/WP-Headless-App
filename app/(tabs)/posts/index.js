2caf15570c6ead9ecb5c0f42cf47f0809a567e4e
Jay Patel <pjay9981@gmail.com>
2023-03-30 22:12:19 +0530

    Minor header changes in 'Posts' Screen

--- app/(tabs)/posts/index.js
+++ app/(tabs)/posts/index.js
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Stack } from "expo-router";
+import { COLORS } from "../../../constants";
 
 const Posts = () => {
 	return (
 		<View>
 			<Stack.Screen
 				options={{
+					headerStyle: { backgroundColor: COLORS.lightWhite },
 					headerShadowVisible: false,
 					headerTitle: "Posts",
 				}}
